a2563a07f89c1cdf790040e9fde3c3de1e453be8
Philippe Plaïa <philippeplaia@gmail.com>
2024-06-27 22:37:58 +0200

    Add substracttemp when we put a tree on the map

--- src/components/temperature-bar/TemperatureBar.js
+++ src/components/temperature-bar/TemperatureBar.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import styles from '../../components/temperature-bar/TemperatureBar.module.css';
 import { fontSize } from '@mui/system';
+import { Vazirmatn } from 'next/font/google';
 
-const TemperatureBar = ({ title, value }) => {
-    // Convert the value from degrees to percentage
+const TemperatureBar = ({ title, value, markers, heatPointId }) => {
+    // Regrouper les données par heatPointId
+    const groupedData = markers.reduce((acc, item) => {
+        const { heatPointId: id } = item;
+        if (!acc[id]) {
+            acc[id] = [];
+        }
+        acc[id].push(item);
+        return acc;
+    }, {});
+
+    console.log(groupedData);
+
+    // Calculer le total de tempreduced pour chaque heatPointId
+    const totals = Object.keys(groupedData).reduce((acc, id) => {
+        const totalTempreduced = groupedData[id].reduce((sum, item) => {
+            return sum + parseFloat(item.tempreduced);
+        }, 0);
+        acc[id] = totalTempreduced;
+        return acc;
+    }, {});
+
+    console.log(totals[heatPointId]);
+
     const minTemp = 15;
     const maxTemp = 40;
-    const percentageValue = ((value - minTemp) / (maxTemp - minTemp)) * 100;
+    let valueSubstract = value;
+
+    if (totals[heatPointId]) {
+        valueSubstract -= totals[heatPointId];
+    }
+
+    const percentageValue = ((valueSubstract - minTemp) / (maxTemp - minTemp)) * 100;
+    console.log(markers);
+    console.log(heatPointId);
+    console.log(percentageValue);
+
+
 
     return (
         <div style={{display: 'flex', alignItems: 'center', gap:26}}>
@@ -25,7 +59,7 @@ const TemperatureBar = ({ title, value }) => {
             </div>
             <div>
                 <div>
-                    <p style={{fontSize: '2rem'}}><strong>{value}°C</strong></p>
+                    <p style={{fontSize: '2rem'}}><strong>{valueSubstract}°C</strong></p>
                 </div>
             </div>
         </div>
