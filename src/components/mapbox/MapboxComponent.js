f3e55f3d93b9ff7f67c4987c58b2adf709c17ace
Tom Engélibert <tomengelibert@MacBook-Pro-de-Tom.local>
2024-09-19 12:43:15 +0200

    fix: symphonie du voyage et sons dambiance

--- src/components/mapbox/MapboxComponent.js
+++ src/components/mapbox/MapboxComponent.js
@@ -12,11 +12,13 @@ export default function MapboxComponent({ setClickedElement, weatherData, setCit
     const mapRef = useRef();
     const [clickedLngLat, setClickedLngLat] = useState(null);
     const [markersWeather, setMarkersWeather] = useState([]);
-    const [activeSoundPlayer, setActiveSoundPlayer] = useState(null);
+    const [ambiancePlayer, setAmbiancePlayer] = useState(null);
+    const [voyagePlayer, setVoyagePlayer] = useState(null);
     const [voyageInterval, setVoyageInterval] = useState(null);
     const [markersList, setMarkersList] = useState([]);
-    const [voyageIndex, setVoyageIndex] = useState(0);
     const [isVoyageStarted, setIsVoyageStarted] = useState(false);
+
+    const isVoyageStartedRef = useRef(isVoyageStarted);
 
     const cities = [
         { name: "Paris", region: "Île-de-France" },
@@ -89,14 +91,14 @@ export default function MapboxComponent({ setClickedElement, weatherData, setCit
 
     const handleClick = useCallback((event) => {
         if (mapRef.current) {
-            if (activeSoundPlayer) {
-                stopActiveSound();
+            if (ambiancePlayer) {
+                stopAmbiancePlayer();
             }
             const features = mapRef.current.queryRenderedFeatures(event.point);
             setClickedElement(features);
             setClickedLngLat(event.lngLat);
         }
-    }, [activeSoundPlayer]);
+    }, []);
 
     const handleDoubleClick = useCallback((event) => {
         const newMarker = {
@@ -109,7 +111,7 @@ export default function MapboxComponent({ setClickedElement, weatherData, setCit
     }, [selectedTree]);
 
     const playSunnyAmbience = () => {
-        setActiveSoundPlayer(new Tone.Player({
+        setAmbiancePlayer(new Tone.Player({
             url: "/sons/sunny-ambiance.mp3",
             loop: true,
             autostart: true,
@@ -118,7 +120,7 @@ export default function MapboxComponent({ setClickedElement, weatherData, setCit
     };
 
     const playRainyAmbience = () => {
-        setActiveSoundPlayer(new Tone.Player({
+        setAmbiancePlayer(new Tone.Player({
             url: "/sons/rain-ambiance.mp3",
             loop: true,
             autostart: true,
@@ -126,161 +128,125 @@ export default function MapboxComponent({ setClickedElement, weatherData, setCit
         }).toDestination());
     };
 
-    const stopActiveSound = () => {
-        console.log(activeSoundPlayer)
-        if (activeSoundPlayer) {
-            activeSoundPlayer.stop();
-            activeSoundPlayer.dispose();
-        }
+    const playSoundForWeather = (weatherType) => {
+        stopAmbiancePlayer();
+
+            switch (weatherType) {
+                case 'Clear':
+                    const sunnySynth = new Tone.Synth().toDestination();
+                    sunnySynth.triggerAttackRelease('C4', '8n');
+                    if (!isVoyageStartedRef.current) {
+                        playSunnyAmbience(); // Only play ambiance if voyage is not started
+                    }
+                    break;
+                case 'Rain':
+                    const rainSynth = new Tone.MembraneSynth().toDestination();
+                    rainSynth.triggerAttackRelease('C2', '8n');
+                    break;
+                case 'Clouds':
+                    const cloudSynth = new Tone.FMSynth().toDestination();
+                    cloudSynth.triggerAttackRelease('E3', '8n');
+                    break;
+                case 'Snow':
+                    const snowSynth = new Tone.Synth({
+                        oscillator: { type: 'triangle' }
+                    }).toDestination();
+                    snowSynth.triggerAttackRelease('G4', '8n');
+                    break;
+                case 'Thunderstorm':
+                    const thunderNoise = new Tone.Noise("pink").start();
+                    const thunderFilter = new Tone.Filter(800, "lowpass").toDestination();
+                    thunderNoise.connect(thunderFilter);
+                    thunderNoise.stop("+0.5");
+                    break;
+                case 'Drizzle':
+                    const drizzleSynth = new Tone.NoiseSynth().toDestination();
+                    drizzleSynth.triggerAttackRelease('8n');
+                    break;
+                case 'Wind':
+                    const windSynth = new Tone.Synth({
+                        oscillator: { type: 'sine' }
+                    }).toDestination();
+                    windSynth.triggerAttackRelease('A3', '8n');
+                    break;
+                default:
+                    const defaultSynth = new Tone.Synth().toDestination();
+                    defaultSynth.triggerAttackRelease('B4', '8n');
+                    break;
+            }
     };
 
-    const playSoundForWeather = (weatherType) => {
-        switch (weatherType) {
-            case 'Clear':
-                const sunnySynth = new Tone.Synth().toDestination();
-                sunnySynth.triggerAttackRelease('C4', '8n');
-                if (!isVoyageStarted){
-                playSunnyAmbience();}
-                else {
-                    stopActiveSound();
-                }
-                break;
-            case 'Rain':
-                const rainSynth = new Tone.MembraneSynth().toDestination();
-                rainSynth.triggerAttackRelease('C2', '8n');
-                break;
-            case 'Clouds':
-                const cloudSynth = new Tone.FMSynth().toDestination();
-                cloudSynth.triggerAttackRelease('E3', '8n');
-                break;
-            case 'Snow':
-                const snowSynth = new Tone.Synth({
-                    oscillator: { type: 'triangle' }
-                }).toDestination();
-                snowSynth.triggerAttackRelease('G4', '8n');
-                break;
-            case 'Thunderstorm':
-                const thunderNoise = new Tone.Noise("pink").start();
-                const thunderFilter = new Tone.Filter(800, "lowpass").toDestination();
-                thunderNoise.connect(thunderFilter);
-                thunderNoise.stop("+0.5");
-                break;
-            case 'Drizzle':
-                const drizzleSynth = new Tone.NoiseSynth().toDestination();
-                drizzleSynth.triggerAttackRelease('8n');
-                break;
-            case 'Wind':
-                const windSynth = new Tone.Synth({
-                    oscillator: { type: 'sine' }
-                }).toDestination();
-                windSynth.triggerAttackRelease('A3', '8n');
-                break;
-            default:
-                const defaultSynth = new Tone.Synth().toDestination();
-                defaultSynth.triggerAttackRelease('B4', '8n');
-                break;
-        }
-    };
 
     useEffect(() => {
         if (isVoyageStarted) {
-           stopActiveSound();
+           stopAmbiancePlayer();
         }
     }, [isVoyageStarted]);
 
-    const startVoyage = async () => {
-        if (markersList.length === 0) return;
+    const startVoyage = () => {
+        // Vérifie si le voyage est déjà commencé avant d'exécuter le reste
+        playPercussionLoop()
+        if (isVoyageStartedRef.current) return;
+
+        // Démarre le voyage
         setIsVoyageStarted(true);
+        isVoyageStartedRef.current = true; // Met à jour la référence
+
+        if (markersList.length === 0) return;
+
         Tone.start().then(() => {
             let localIndex = 0;
-    
-            playPercussionLoop(); // Démarrer la boucle de percussions ici
-    
-            const intervalId = setInterval(async () => {
+
+            // Assurez-vous d'arrêter le voyage en cours s'il existe déjà un intervalle
+            if (voyageInterval) {
+                clearInterval(voyageInterval);
+                setVoyageInterval(null);
+            }
+
+            const intervalId = setInterval(() => {
                 if (localIndex >= markersList.length) {
                     clearInterval(intervalId);
                     setVoyageInterval(null);
-                    stopActiveSound(); // Arrêter la boucle de percussions
+                    stopVoyagePlayer();  // Arrêter la boucle de percussions
+                    setIsVoyageStarted(false);  // Voyage terminé
+                    isVoyageStartedRef.current = false; // Met à jour la référence
                     return;
                 }
-    
+
                 const marker = markersList[localIndex];
-                console.log('Fetching weather data for marker:', marker);
-    
-                try {
-                    const cityResponse = await apiManager.getCityByLngLat(marker.longitude, marker.latitude, tokenMapbox);
-                    const cityName = cityResponse.features[0].properties.name;
-                    console.log('City Name:', cityName);
-    
-                    const weatherData = await apiManager.getWeatherByCity(cityName, tokenOWeather);
-                    console.log('Weather Data:', weatherData);
-    
-                    if (weatherData && weatherData.weather && weatherData.weather.length > 0) {
-                        playSoundForWeather(weatherData.weather[0].main);
-                    } else {
-                        console.error('Weather data is not in the expected format:', weatherData);
-                    }
-    
-                    mapRef.current?.flyTo({
-                        center: [marker.longitude, marker.latitude],
-                        zoom: 10,
+
+                apiManager.getCityByLngLat(marker.longitude, marker.latitude, tokenMapbox)
+                    .then(cityResponse => {
+                        const cityName = cityResponse.features[0].properties.name;
+                        return apiManager.getWeatherByCity(cityName, tokenOWeather);
+                    })
+                    .then(weatherData => {
+                        if (weatherData && weatherData.weather && weatherData.weather.length > 0) {
+                            playSoundForWeather(weatherData.weather[0].main);
+                        } else {
+                            console.error('Weather data is not in the expected format:', weatherData);
+                        }
+
+                        mapRef.current?.flyTo({
+                            center: [marker.longitude, marker.latitude],
+                            zoom: 10,
+                        });
+
+                        localIndex++;
+                    })
+                    .catch(error => {
+                        console.error('Error fetching weather data:', error);
                     });
-    
-                    localIndex++;
-                    setVoyageIndex(localIndex);
-                } catch (error) {
-                    console.error('Error fetching weather data:', error);
-                }
-            }, 2000);
+            }, 2000);  // Intervalle de 2 secondes entre chaque marker
+
             setVoyageInterval(intervalId);
         });
     };
-    
-    const playGermanRhythm = () => {
-        // Définir les instruments
-        const bassDrum = new Tone.MembraneSynth().toDestination();
-        const snareDrum = new Tone.NoiseSynth({
-            noise: { type: 'white' },
-            envelope: { attack: 0.01, decay: 0.1, sustain: 0.1, release: 0.1 }
-        }).toDestination();
-        const hiHat = new Tone.MetalSynth({
-            frequency: 2000,
-            envelope: {
-                attack: 0.01,
-                decay: 0.1,
-                sustain: 0.1,
-                release: 0.1
-            }
-        }).toDestination();
-        const melodySynth = new Tone.Synth().toDestination(); // Synthétiseur pour la mélodie
-    
-        // Séquence de batterie Disco
-        const discoBeat = new Tone.Sequence((time, step) => {
-            // Jouer la grosse caisse sur les temps forts (0, 4, 8, 12)
-            if (step % 4 === 0) {
-                bassDrum.triggerAttackRelease("C2", "8n", time);
-            }
-            // Jouer la caisse claire sur les temps 2 et 6
-            if (step % 4 === 2) {
-                snareDrum.triggerAttackRelease("8n", time);
-            }
-            // Motif de cymbale Hi-Hat sur les temps 0, 1, 2
-            if (step % 4 === 0 || step % 4 === 1 || step % 4 === 2) {
-                hiHat.triggerAttackRelease("8n", time);
-            }
-        }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "8n");
-    
-        // Démarrer la séquence et le transport
-        discoBeat.start(0);
-        Tone.Transport.bpm.value = 120; // Définir les BPM (ajuster si besoin)
-        Tone.Transport.start();
-    
-        return discoBeat; // Retourner la séquence pour l'arrêter plus tard si nécessaire
-    };
-    
 
-    
-    
+
+
+
+
     const playPercussionLoop = () => {
         const kick = new Tone.MembraneSynth().toDestination();
         const snare = new Tone.NoiseSynth({
@@ -310,102 +276,35 @@ export default function MapboxComponent({ setClickedElement, weatherData, setCit
             }
         }, ['kick', 'hihat', 'snare', 'hihat'], 'up').start(0);
     
-        Tone.Transport.bpm.value = 120; // Vitesse du rythme (BPM)
+        Tone.Transport.bpm.value = 120;
         Tone.Transport.start();
-        setActiveSoundPlayer(pattern); // Pour arrêter plus tard
+        setVoyagePlayer(pattern);
     };
     
     const stopVoyage = () => {
-        if (voyageInterval) {
-            clearInterval(voyageInterval);
+            stopVoyagePlayer();
             setVoyageInterval(null);
-        }
-        stopActiveSound(); // Arrêter la boucle de percussions ici aussi
+            setIsVoyageStarted(false);
+            setMarkersList([])
+
     };
-    
-    const getRhythmForCountry = (countryName) => {
-        switch (countryName) {
-            case 'France':
-                return playFrenchRhythm();
-            case 'Germany':
-                return playGermanRhythm();
-            case 'Spain':
-                return playSpanishRhythm();
-            // Ajouter plus de pays ici
-            default:
-                return playDefaultRhythm();
+
+    const stopAmbiancePlayer = () => {
+        if (ambiancePlayer) {
+            ambiancePlayer.stop();
+            ambiancePlayer.disconnect();
         }
     };
-    
-    const playFrenchRhythm = () => {
-        const kick = new Tone.MembraneSynth().toDestination();
-        const snare = new Tone.NoiseSynth({
-            noise: { type: 'white' },
-            envelope: { attack: 0.005, decay: 0.1, sustain: 0 }
-        }).toDestination();
-        const hiHat = new Tone.MetalSynth({
-            frequency: 400,
-            envelope: {
-                attack: 0.001,
-                decay: 0.1,
-                release: 0.01,
-            },
-            harmonicity: 5.1,
-            modulationIndex: 32,
-            resonance: 8000,
-            octaves: 1.5,
-        }).toDestination();
-    
-        const pattern = new Tone.Pattern((time, note) => {
-            if (note === 'kick') {
-                kick.triggerAttackRelease('C2', '8n', time);
-            } else if (note === 'snare') {
-                snare.triggerAttackRelease('8n', time);
-            } else if (note === 'hihat') {
-                hiHat.triggerAttackRelease('16n', time);
-            }
-        }, ['kick', 'hihat', 'snare', 'hihat'], 'up').start(0);
-    
-        Tone.Transport.bpm.value = 120; // Vitesse du rythme (BPM)
-        Tone.Transport.start();
-        return pattern;
-    };
-    
-    const playSpanishRhythm = () => {
-        const snare = new Tone.NoiseSynth().toDestination();
-        const hiHat = new Tone.MetalSynth().toDestination();
-        
-        const pattern = new Tone.Pattern((time, note) => {
-            if (note === 'snare') {
-                snare.triggerAttackRelease('8n', time);
-            } else if (note === 'hihat') {
-                hiHat.triggerAttackRelease('16n', time);
-            }
-        }, ['snare', 'hihat'], 'up').start(0);
-    
-        Tone.Transport.bpm.value = 130; // BPM spécifique à l'Espagne
-        Tone.Transport.start();
-        return pattern;
-    };
-    
-    const playDefaultRhythm = () => {
-        const kick = new Tone.MembraneSynth().toDestination();
-        const pattern = new Tone.Loop((time) => {
-            kick.triggerAttackRelease('C1', '8n', time);
-        }, '1n').start(0);
-    
-        Tone.Transport.bpm.value = 110; // BPM par défaut
-        Tone.Transport.start();
-        return pattern;
-    };
-    
-    const stopCurrentRhythm = () => {
-        if (activeSoundPlayer) {
-            activeSoundPlayer.stop();
-            activeSoundPlayer.dispose();
+
+    const stopVoyagePlayer = () => {
+        if (voyagePlayer) {
+            voyagePlayer.stop();
+            voyagePlayer.dispose();
         }
     };
-    
+
+
+
     return (
         <div style={{ position: 'relative', height: '100vh' }}>
             <div style={{ position: 'absolute', top: 10, left: 10, zIndex: 1, backgroundColor: 'white', padding: '10px', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
