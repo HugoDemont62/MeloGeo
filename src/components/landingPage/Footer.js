a1b776d3524165f2250c6f17c23bd14d451bee46
Tom Engélibert <tomengelibert@MacBook-Pro-de-Tom.local>
2024-09-20 09:33:06 +0200

    ajout credit ux

--- src/components/landingPage/Footer.js
+++ src/components/landingPage/Footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.content}>
-            <p>&copy; Fait par Philippe, Tom et Hugo</p>
+            <p>&copy; Fait par Philippe Plaïa, Tom Engélibert et Hugo Dumont</p>
+            <p>Basé sur un design réalisé par Agathe Moreau, Mathilde Fenart et Baptiste Blicq</p>
             </div>
             <div className={styles.content}>
             <p>&copy;  2024. Touts droits réservé</p>
