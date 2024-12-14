011ba678f70a5052d2ad8a25ff831519845f1872
HugoDemont <hugodemont62@hotmail.fr>
2024-12-14 14:09:20 +0100

    [FEAT] Add animation for all the sectins

--- src/components/landingPage/Section.js
+++ src/components/landingPage/Section.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Section.module.css';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-const Section = ({ title, description, imageUrl, reverse }) => (
-  <div id="sensibiliser" className={`${styles.sectionContainer} ${reverse ? styles.flexRowReverse : ''}`}>
-    <div className={styles.content}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
+const Section = ({ title, description, imageUrl, reverse }) => {
+  const sectionRef = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animation lors de l'apparition de la section
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0, y: 100 }, // Initial : masqué et légèrement décalé en bas
+      {
+        opacity: 1,
+        y: 0, // Final : complètement visible à sa position d'origine
+        duration: 1, // Durée de l'animation
+        ease: "power3.out", // Transition fluide
+        scrollTrigger: {
+          trigger: sectionRef.current, // Déclencheur : La section elle-même
+          start: "top 80%", // Début de l'animation : lorsque le haut de la section entre dans le viewport
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <div
+      ref={sectionRef}
+      id="sensibiliser"
+      className={`${styles.sectionContainer} ${reverse ? styles.flexRowReverse : ''}`}
+    >
+      <div className={styles.content}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+      </div>
+      <div className={styles.image}>
+        <img src={imageUrl} alt={`Image for ${title}`} className={styles.imageElement} />
+      </div>
     </div>
-    <div className={styles.image}>
-      <img src={imageUrl} alt={`Image for ${title}`} className={styles.imageElement} />
-    </div>
-  </div>
-);
+  );
+};
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
--- src/components/landingPage/Section3.js
+++ src/components/landingPage/Section3.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Section3.module.css';
+import React, { useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import styles from "./Section3.module.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const Section3 = ({ title, description, imageUrl, reverse }) => (
-  <div id="anticiper" className={`${styles.sectionContainer} ${reverse ? styles.flexRowReverse : ''}`}>
-    <div className={styles.content}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
+const Section3 = ({ title, description, imageUrl, reverse }) => {
+  const sectionRef = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animation d'entrée au défilement
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0, y: 100 }, // Initial : masqué avec un décalage vertical
+      {
+        opacity: 1,
+        y: 0, // Final : visible et aligné
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top 80%",
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <div
+      ref={sectionRef}
+      id="anticiper"
+      className={`${styles.sectionContainer} ${reverse ? styles.flexRowReverse : ""}`}
+    >
+      <div className={styles.content}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+      </div>
+      <div className={styles.image}>
+        <img src={imageUrl} alt={`Image for ${title}`} className={styles.imageElement} />
+      </div>
     </div>
-    <div className={styles.image}>
-      <img src={imageUrl} alt={`Image for ${title}`} className={styles.imageElement} />
-    </div>
-  </div>
-);
+  );
+};
 
 Section3.propTypes = {
   title: PropTypes.string.isRequired,
